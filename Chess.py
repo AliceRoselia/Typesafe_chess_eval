1f7f9c6ebaa31ee1cd6c4de48f7395beee2936d7
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-09-17 11:58:48 +0700

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -18,7 +18,7 @@ with TypeSafeClient(retry=RetryPolicy(max_retries=3, backoff_max=0.2, timeout=1.
             questions={
                 "evaluation": Choice(instructions = "What is your game outcome assuming neither side makes major mistake?",
                                      criteria = {"win":None, "draw":None, "lose":None}),
-                "best move": Choice(instructions = "What is your best move for the current position assuming neither side makes major mistake?",
+                "best move": Choice(instructions = "What is your best move for the current position assuming neither side makes major mistake? The answers are in uci notation, not algebraic notation.",
                                     criteria = {i.uci():None for i in board.legal_moves})
             }
         )
@@ -31,3 +31,8 @@ with TypeSafeClient(retry=RetryPolicy(max_retries=3, backoff_max=0.2, timeout=1.
 
 print("Replay:")
 print(chess.pgn.Game.from_board(board))
+
+"""
+Example_game:
+1. e4 e5 2. d4 Nf6 3. Nc3 Nxe4 4. dxe5 d5 5. exd6 Nxd6 6. Nf3 Ne4 7. Nxe4 Nc6 8. Ne5 Nd4 9. Nxf7 Ne6 10. Ne5 Nd4 11. Nf7 Ne6 12. Ne5 Nd4 13. Nf7 Ne6 14. Ne5 Nd4 15. Nf7 Ne6 16. Ne5 Nd4 17. Nf7 1/2-1/2  
+"""
